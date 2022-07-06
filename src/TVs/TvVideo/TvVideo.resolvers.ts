4271f12d58a18785399ae7a1744b61bdb333fb99
JANGTAEHEE <jangth0655@gmail.com>
2022-07-06 22:56:09 +0900

    updated_TvVidieo

---
+++ src/TVs/TvVideo/TvVideo.resolvers.ts
@@ -0,0 +1,14 @@
+const resolvers = {
+  Query: {
+    TvVideo: async (_, { id }) => {
+      const response = await (
+        await fetch(
+          `https://api.themoviedb.org/3/tv/${id}/videos?api_key=${process.env.API_KEY}&language=ko`
+        )
+      ).json();
+      return response;
+    },
+  },
+};
+
+export default resolvers;
