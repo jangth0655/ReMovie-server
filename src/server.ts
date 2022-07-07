2eb4524734eee884047b86bcbe954075b11b93ba
JANGTAEHEE <jangth0655@gmail.com>
2022-07-07 21:23:13 +0900

    server_plugin_playgournd

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { ApolloServer } from "apollo-server";
 import { resolvers, typeDefs } from "./schema";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+});
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
