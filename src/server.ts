fce34d0f8835cb3ac97cf09218fd06e5e27f9798
JANGTAEHEE <jangth0655@gmail.com>
2022-07-08 00:58:52 +0900

    server_apollo-express

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,33 @@
 import "dotenv/config";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { resolvers, typeDefs } from "./schema";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import express from "express";
+import http from "http";
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
-});
+const PORT = process.env.PORT;
 
-server.listen().then(({ url }) => {
-  console.log(`Running on ${url}`);
-});
+async function startApolloServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    introspection: true,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: PORT }, resolve)
+  );
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+}
+startApolloServer();
+
+/*   server.listen().then(({ url }) => {
+    console.log(`Running on ${url}`);
+  }); */
