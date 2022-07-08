9a73931c97ab20c2301d393d10b8bd077e14c259
JANGTAEHEE <jangth0655@gmail.com>
2022-07-08 09:14:07 +0900

    upcomming_test

--- src/movies/upcomingMovies/upcomingMovies.resolvers.ts
+++ src/movies/upcomingMovies/upcomingMovies.resolvers.ts
@@ -2,12 +2,11 @@ const resolvers = {
   Query: {
     upcomingMovies: async () => {
       try {
-        const response = await (
-          await fetch(
-            `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=ko`
-          )
-        ).json();
-        return response;
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=ko`
+        );
+        console.log(response);
+        return await response.json();
       } catch (e) {
         console.log(e);
         return;
